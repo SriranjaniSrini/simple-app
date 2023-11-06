8273a5e4953b23f29a06af7fc243ae8d9eb0b6fa
SriranjaniSrini <sriranjanis.srinivasan@gmail.com>
2023-11-06 14:34:49 +0100

    update script.js
    Created and arrwy for the pokemon list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-alert('hello world');
-let favoriteFood = 'sweets';
-document.write(favoriteFood);
+let pokemonList= [{name:"balbasure",height:7,type:["grass","water"]},
+                  {name:"squirtle",height:0.5,type:"water"},
+                  {name:"eve",height:8,type:"water"}];
